72834b245021d47f6ea6642928f5911b4e47391a
Robin <robinson.1701625954@ucaldas.edu.co>
2023-04-10 09:16:45 -0500

    entity inconvenients end

---
+++ Domain/Entities/Inconvenients.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Inconvenients
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public string? State { get; set; }
+        [Required]
+        public int? DaysDelay { get; set; }
+        [Required]
+        public int ServiceRequesedId { get; set; }
+        [Required]
+        public bool Seen { get; set; }
+
+        public string? Description { get; set; }
+
+        public ICollection<Request>? Requests { get; set; }
+
+    }
+}
