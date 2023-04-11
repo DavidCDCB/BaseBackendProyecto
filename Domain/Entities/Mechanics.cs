7b29b4832513718946eddb3f0722eda29a2ca1d3
Robin <robinson.1701625954@ucaldas.edu.co>
2023-04-11 09:30:04 -0500

    entity mechanics and payrolls end

---
+++ Domain/Entities/Mechanics.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Mechanics
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? SurName { get; set; }
+        [Required]
+        public string? Phone { get; set; }
+        [Required]
+        public string? Role { get; set; }
+        [Required]
+        public string? Email { get; set; }
+        [Required]
+        public string? Address { get; set; }
+        [Required]
+        public double? Commission { get; set; }
+        [Required]
+        public double? Salary { get; set; }
+        [Required]
+        public int? UserId { get; set; }
+        public ICollection<Request>? Requests { get; set; }
+    }
+}
---
+++ Domain/Entities/Payrolls.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Payrolls
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public DateTime? StartDate { get; set; }
+        [Required]
+        public DateTime? EndDate { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        public double? Accruals { get; set; }
+        [Required]
+        public double? Deductions { get; set; }
+        [Required]
+        public double? Settlement { get; set;}
+
+        public ICollection<Request>? Requests { get; set; }
+    }
+}
