8c622f1121544546b642a07f63bf8a16f55b00d5
DavidCDCB <cristian-cb1@hotmail.com>
2023-04-13 16:51:53 -0500

    Cambio de nombres de tablas y campos

--- Domain/Entities/Base/User.cs
+++ Domain/Entities/Base/User.cs
@@ -4,18 +4,23 @@ using System.ComponentModel.DataAnnotations.Schema;
 // https://www.entityframeworktutorial.net/code-first/column-dataannotations-attribute-in-code-first.aspx
 namespace Domain.Entities.Base
 {
-    public class User
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-        [Required]
-        public string FullName { get; set; }
-        [Required]
-        public string Email { get; set; }
-        [Required]
-        public long Phone { get; set; }
+  [Table("users")]
+  public class User
+  {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
+    [Required]
+    [Column("full_name")]
+    public string FullName { get; set; }
+    [Required]
+    [Column("email")]
+    public string Email { get; set; }
+    [Required]
+    [Column("phone")]
+    public long Phone { get; set; }
 
-        public ICollection<Course>? Courses { get; set; }
-    }
+    public ICollection<Course>? Courses { get; set; }
+  }
 }
