8f4cf185b604f9ede974d07df9bcca6e775516f1
DavidCDCB <cristian-cb1@hotmail.com>
2023-04-14 11:46:39 -0500

    Endpoints CRUD de proveedores

--- NewProtoNet/Controllers/SupplierController.cs
+++ NewProtoNet/Controllers/SupplierController.cs
@@ -2,16 +2,76 @@
 using Microsoft.AspNetCore.Mvc;
 using RestServer.Interfaces;
 using Domain.Entities.Base;
+using Domain.Entities;
 
 namespace RestServer.Controllers
 {
-  public class SupplierController
+  [ApiController]
+  [Route("api/[controller]")] // https://localhost:7204/api/User
+  public class SupplierController : Controller
   {
     private readonly ISupplierRepository supplierRepository;
 
     public SupplierController(ISupplierRepository supplierRepository)
     {
       this.supplierRepository = supplierRepository;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult> GetSuppliers()
+    {
+      return Ok(await this.supplierRepository.GetSuppliers());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetSupplier(int id)
+    {
+      Supplier? encontrado = await this.supplierRepository.GetSupplier(id);
+
+      if (encontrado == null)
+      {
+        return NotFound();
+      }
+
+      return Ok(encontrado);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> PostSupplier(SupplierDTO supplierDTO)
+    {
+      try
+      {
+        return Ok(await this.supplierRepository.PostSupplier(supplierDTO));
+      }
+      catch (Exception)
+      {
+        return BadRequest();
+      }
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutSupplier(int id, SupplierDTO supplierDTO)
+    {
+      Supplier actualizado = await this.supplierRepository.UpdateSupplier(id, supplierDTO);
+
+      if (actualizado == null)
+      {
+        return NotFound();
+      }
+      return Ok(actualizado);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> RemoveSupplier(int id)
+    {
+      Supplier eliminado = await this.supplierRepository.DeleteSupplier(id);
+
+      if (eliminado == null)
+      {
+        return NotFound();
+      }
+      return Ok(eliminado);
+
     }
   }
 }
