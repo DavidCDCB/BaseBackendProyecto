fa46a2f6042a09ac2e5cf7b4b68a22c02cd095cd
DavidCDCB <cristian-cb1@hotmail.com>
2023-04-14 09:47:52 -0500

    Ajuste de nombre de campos y tablas

--- Domain/Entities/Service.cs
+++ Domain/Entities/Service.cs
@@ -3,19 +3,24 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
+  [Table("services")]
   public class Service
   {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
     public int Id { get; set; }
-
     [Required]
+    [Column("name")]
     public string? Name { get; set; }
     [Required]
+    [Column("price")]
     public double? Price { get; set; }
     [Required]
+    [Column("description")]
     public string? Description { get; set; }
     [Required]
+    [Column("category")]
     public string? Category { get; set; }
 
     public ICollection<Request>? Requests { get; set; }
