2ef00691b0a748b3d90d0c5d495ee5d150483c8e
DavidCDCB <cristian-cb1@hotmail.com>
2023-04-14 09:47:52 -0500

    Ajuste de nombre de campos y tablas

--- Domain/Entities/Supplier.cs
+++ Domain/Entities/Supplier.cs
@@ -3,24 +3,33 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
+  [Table("suppliers")]
   public class Supplier
   {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
     public int Id { get; set; }
     [Required]
+    [Column("nit")]
     public string? Nit { get; set; }
     [Required]
+    [Column("name")]
     public string? Name { get; set; }
     [Required]
+    [Column("company")]
     public string? Company { get; set; }
     [Required]
+    [Column("surname")]
     public string? SurName { get; set; }
     [Required]
+    [Column("phone")]
     public string? Phone { get; set; }
     [Required]
+    [Column("email")]
     public string? Email { get; set; }
     [Required]
+    [Column("address")]
     public string? Address { get; set; }
 
     // Compras ...
--- Domain/Entities/Vehicle.cs
+++ Domain/Entities/Vehicle.cs
@@ -3,22 +3,29 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Domain.Entities
 {
+  [Table("vehicles")]
   public class Vehicle
   {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
     public int Id { get; set; }
     [Required]
+    [Column("plate")]
     public string? Plate { get; set; }
     [Required]
+    [Column("model")]
     public string? Model { get; set; }
     [Required]
+    [Column("year")]
     public string? Year { get; set; }
+    [Column("description")]
     public string? Description { get; set; }
     [Required]
+    [Column("color")]
     public string? Color { get; set; }
 
-
+    [Column("client_id")]
     public int? ClientId { get; set; }
     public Client? Client { get; set; }
   }
