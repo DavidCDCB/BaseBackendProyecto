b2bce77c4d0c4ba986ccb4db4f7bdecccb0c2ece
DavidCDCB <cristian-cb1@hotmail.com>
2023-04-16 15:34:36 -0500

    Paginación en Clientes y Servicios

--- NewProtoNet/Interfaces/IServiceRepository.cs
+++ NewProtoNet/Interfaces/IServiceRepository.cs
@@ -6,6 +6,7 @@ namespace RestServer.Interfaces
   public interface IServiceRepository
   {
     Task<List<Service>> GetServices();
+    Task<List<Service>> GetByPage(int page);
     Task<Service?> GetService(int id);
     Task<Service> PostService(ServiceDTO service);
     Task<Service?> UpdateService(int id, ServiceDTO service);
