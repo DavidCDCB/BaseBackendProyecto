0d9f748125f265cfb732e54d1b16f42de1418304
Robin <robinson.1701625954@ucaldas.edu.co>
2023-04-17 09:57:00 -0500

    cambio nombre payrolls columnas

--- Domain/Entities/Payroll.cs
+++ Domain/Entities/Payroll.cs
@@ -8,22 +8,30 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
+    [Table("payrolls")]
     public class Payroll
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
         public int Id { get; set; }
         [Required]
+        [Column("star_date")]
         public DateOnly? StarDate { get; set; }
         [Required]
+        [Column("end_date")]
         public DateOnly? EndDate { get; set; }
         [Required]
+        [Column("description")]
         public string? Description { get; set; }
         [Required]
+        [Column("accruals")]
         public double? Accruals { get; set; }
         [Required]
+        [Column("deductions")]
         public double? Deductions { get; set; }
         [Required]
+        [Column("settlement")]
         public double? Settlement { get; set; }
 
         
