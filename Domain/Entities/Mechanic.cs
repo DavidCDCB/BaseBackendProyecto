61103257e55ad77fc070c431a0701e2bb34461f9
Robin <robinson.1701625954@ucaldas.edu.co>
2023-04-17 09:51:16 -0500

    cambio nombre mechanic columnas

--- Domain/Entities/Mechanic.cs
+++ Domain/Entities/Mechanic.cs
@@ -8,28 +8,39 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
+    [Table("mechanics")]
     public class Mechanic
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
         public int Id { get; set; }
         [Required]
+        [Column("name")]
         public string? Name { get; set; }
         [Required]
+        [Column("surname")]
         public string? SurName { get; set; }
         [Required]
+        [Column("phone")]
         public string? Phone { get; set; }
         [Required]
+        [Column("role")]
         public string? Role { get; set; }
         [Required]
+        [Column("email")]
         public string? Email { get; set; }
         [Required]
+        [Column("address")]
         public string? Address { get; set; }
         [Required]
+        [Column("commission")]
         public double? Commission { get; set; }
         [Required]
+        [Column("salary")]
         public double? Salary { get; set; }
         [Required]
+        [Column("user_id")]
         public int? UserId { get; set; }
 
         public ICollection<Payroll>? Payrolls { get; set; }
