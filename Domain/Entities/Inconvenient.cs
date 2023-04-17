6e355b70aa1e9408ab2fdf3a2e6c717630dc5b4f
Robin <robinson.1701625954@ucaldas.edu.co>
2023-04-17 09:54:14 -0500

    cambio nombre inconvenients columnas

--- Domain/Entities/Inconvenient.cs
+++ Domain/Entities/Inconvenient.cs
@@ -8,23 +8,32 @@ using System.Threading.Tasks;
 
 namespace Domain.Entities
 {
+    [Table("inconvenients")]
     public class Inconvenient
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
         public int Id { get; set; }
         [Required]
+        [Column("date_act")]
         public DateOnly? DateAct { get; set; }
         [Required]
+        [Column("state")]
         public string? State { get; set; }
         [Required]
+        [Column("days_delay")]
         public int? DaysDelay { get; set; }
         [Required]
+        [Column("service_request_id")]
         public int ServiceRequesedId { get; set; }
         [Required]
+        [Column("seen")]
         public bool Seen { get; set; }
         [Required]
+        [Column("description")]
         public string? Description { get; set; }
+        [Column("request_id")]
         public int? RequestID { get; set; }
         public Request? Request { get; set; }
 
