4a27422b333b375544a5b74c81ef5f1f57793e7c
CristianDGuerreroA <69438010+CristianDGuerreroA@users.noreply.github.com>
2023-05-22 11:04:53 -0500

    Se modifican los métodos de encriptación.

--- NewProtoNet/Repositories/UserRepository.cs
+++ NewProtoNet/Repositories/UserRepository.cs
@@ -98,23 +98,6 @@ namespace RestServer.Repositories
             }
         }
 
-        public string DecodeFrom64(string encodedData)
-        {
-            try
-            {
-                System.Text.UTF8Encoding encoder = new System.Text.UTF8Encoding();
-                System.Text.Decoder utf8Decode = encoder.GetDecoder();
-                byte[] todecode_byte = Convert.FromBase64String(encodedData);
-                int charCount = utf8Decode.GetCharCount(todecode_byte, 0, todecode_byte.Length);
-                char[] decoded_char = new char[charCount];
-                utf8Decode.GetChars(todecode_byte, 0, todecode_byte.Length, decoded_char, 0);
-                string result = new String(decoded_char);
-                return result;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("Error in DecodeFrom64" + ex.Message);
-            }          
-        }
+        
     }
 }
